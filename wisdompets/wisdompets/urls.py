b48101cbee38053f52bbc4fa341e6bd9e51db6d9
JashonMenya <jjmenya14@gmail.com>
2024-01-25 16:51:40 -0600

    getting started with view and urls

--- wisdompets/wisdompets/urls.py
+++ wisdompets/wisdompets/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from adoptions import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('adoptions/<int:pet_id>/', views.pet_detail, name='pet_detail'),
+    path('admin/', admin.site.urls),
 ]
