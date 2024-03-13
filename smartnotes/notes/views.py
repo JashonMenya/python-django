d967121468808bbfb6e76141c7e92ee9d6ab7db2
JashonMenya <jjmenya14@gmail.com>
2024-03-13 18:57:45 -0500

    working examples urls + views

--- smartnotes/notes/views.py
+++ smartnotes/notes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Notes
+
+
+def list(request):
+    all_notes = Notes.objects.all()
+    return render(request, "notes/notes.html", {"notes": all_notes})
