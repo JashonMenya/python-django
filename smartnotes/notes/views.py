edd299b3b79ad64d897dcb31a1bb8551a37dd9ee
JashonMenya <jjmenya14@gmail.com>
2024-03-16 11:02:25 -0500

    class based views for notes

--- smartnotes/notes/views.py
+++ smartnotes/notes/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render
 from django.http import Http404
+from django.views.generic import ListView, DetailView
 
 from .models import Notes
 
 
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, "notes/notes.html", {"notes": all_notes})
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = "notes"
+    # optional
+    template_name = "notes/notes_list.html"
 
-def detail(request, pk):
-    try:
-        note = Notes.objects.get(pk=pk)
-    except Notes.DoesNotExist:
-        raise Http404(f"Note with primary key:{pk},Does not Exist")
-        
-    return render(request, 'notes/notes_details.html', {'note': note})
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
+    template_name = "notes/notes_detail.html"
+
+
+# def list(request):
+#     all_notes = Notes.objects.all()
+#     return render(request, "notes/notes.html", {"notes": all_notes})
+
+
+# def detail(request, pk):
+#     try:
+#         note = Notes.objects.get(pk=pk)
+#     except Notes.DoesNotExist:
+#         raise Http404(f"Note with primary key:{pk},Does not Exist")
+
+#     return render(request, "notes/notes_details.html", {"note": note})
