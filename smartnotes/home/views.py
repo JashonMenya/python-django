ab807ad13c7142e7fb93c1bb575dce1b3aba226d
JashonMenya <jjmenya14@gmail.com>
2024-03-17 14:00:31 -0500

    sign up page can only be accessed by users not signed in

--- smartnotes/home/views.py
+++ smartnotes/home/views.py
@@ -10,6 +10,7 @@ from django.shortcuts import redirect
 from django.views import View
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
 
 
 
@@ -17,6 +18,11 @@ class SignupView(CreateView):
     form_class = UserCreationForm
     template_name = 'home/register.html'
     success_url = '/smart/notes'
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('notes.list')
+        return super().get(request, *args, **kwargs)
 
 class LogoutInterfaceView(View):
     def get(self, request, *args, **kwargs):
